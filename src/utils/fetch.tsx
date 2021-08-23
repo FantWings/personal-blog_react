86dcd83ff094119b41d99467006147502a2f08e9
小翼 <zhijun@furry.top>
2021-08-24 00:55:45 +0400

    改进一下fetch

--- src/utils/fetch.tsx
+++ src/utils/fetch.tsx
@@ -13,13 +13,13 @@ const fetchData = (url: string, method: 'GET' | 'POST' | 'PUT', headers?: object
         if (!ok) {
           switch (status) {
             case 400:
-              return Promise.reject('请求包含语法错误或无法完成请求！')
+              return Promise.reject({ reason: '请求包含语法错误或无法完成请求！', status })
             case 404:
-              return Promise.reject('请求的资源未找到！')
+              return Promise.reject({ reason: '请求的资源未找到！', status })
             case 500:
-              return Promise.reject('服务器在处理请求的过程中发生了错误')
+              return Promise.reject({ reason: '服务器在处理请求的过程中发生了错误', status })
             default:
-              return Promise.reject('未知错误，请求失败')
+              return Promise.reject({ reason: '未知错误，请求失败', status })
           }
         } else {
           return response.json()
@@ -39,20 +39,20 @@ const fetchData = (url: string, method: 'GET' | 'POST' | 'PUT', headers?: object
         switch (status) {
           case 1:
             message.error(msg)
-            throw status
+            return Promise.reject({ status, msg })
           case 2:
             message.warn(msg)
-            throw status
+            return Promise.reject({ status, msg })
           case 10:
             localStorage.clear()
             message.warn('登录状态失效，请刷新页面')
-            throw status
+            return Promise.reject(status)
           default:
-            return data
+            return Promise.resolve(data)
         }
       },
-      (reason) => {
-        message.error(reason)
+      ({ reason, status }) => {
+        message.error(`${reason} - ${status}`)
       }
     )
 }
