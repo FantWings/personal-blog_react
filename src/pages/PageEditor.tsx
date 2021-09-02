6a05a7a195892278ff1e1e279471ddcec9872876
FantWings <zhijun@furry.top>
2021-09-02 10:23:22 +0400

    撰写文章额外的tag、封面图字段

--- src/pages/PageEditor.tsx
+++ src/pages/PageEditor.tsx
@@ -12,14 +12,20 @@ import { ThemeColor } from '../utils/constent'
 import styled from 'styled-components'
 import { message } from 'antd'
 import { blogDetailRespond } from '../utils/interfaces'
+import { CloseOutlined } from '@ant-design/icons'
 
 export default function PageEditer() {
   const location = useLocation<{ edit: boolean; archId: number | undefined }>()
   const [content, setContent] = useState('')
   const [title, setTitle] = useState<string | undefined>('')
+  const [tags, setTags] = useState<Array<string>>([])
+  const [tagsFormText, setTagsFromText] = useState<string>('')
+  const [coverImage, setCoverImage] = useState('')
+
   const { edit, archId } = location.state
   const history = useHistory()
 
+  // 初始加载函数，用来向服务器获取要修改的文章对应的数据
   useEffect(() => {
     edit &&
       fetchData(`${BASEURL}/api/v1/archive/getDetail/${archId}`, 'GET')
@@ -32,8 +38,9 @@ export default function PageEditer() {
         })
   }, [edit, archId, history])
 
+  // 新建模式提交函数，点击提交后，将数据收集给服务器
   const HandleSubmit = () => {
-    const body = { title, content, cover_image: '', time_for_read: 5 }
+    const body = { title, content, cover_image: coverImage, time_for_read: 5, tags }
     fetchData(`${BASEURL}/api/v1/archive/add`, 'POST', { token: localStorage.getItem('token') }, body)
       .then(() => {
         message.success('操作成功')
@@ -44,8 +51,9 @@ export default function PageEditer() {
       })
   }
 
+  // 修改模式提交函数，点击提交后，将数据收集给服务器
   const HandleUpdate = () => {
-    const body = { title, content, cover_image: '', time_for_read: 5 }
+    const body = { title, content, cover_image: coverImage, time_for_read: 5, tags }
     fetchData(
       `${BASEURL}/api/v1/archive/update?archId=${archId}`,
       'POST',
@@ -61,6 +69,28 @@ export default function PageEditer() {
       })
   }
 
+  // 标签添加函数，监听回车键，将新tag插入到tags状态中
+  const handleKeyPress = ({ charCode }: { charCode: number }) => {
+    if (charCode === 13) {
+      setTags((prev) => {
+        if (prev.indexOf(tagsFormText) < 0) {
+          prev.splice(0, 0, tagsFormText)
+        }
+        return [...prev]
+      })
+      setTagsFromText('')
+    }
+  }
+
+  // 标签删除函数，从tags状态里删除对应的tag
+  const handleDeleteTagFromTagGroup = (index: number) => {
+    setTags((prev) => {
+      prev.splice(index, 1)
+      return [...prev]
+    })
+  }
+
+  // 页面主体
   return (
     <div style={{ width: '100%' }}>
       <EditContainer>
@@ -84,13 +114,36 @@ export default function PageEditer() {
           style={{ height: '600px' }}
         />
         <div id="extraInfo">
-          <div id="coverImage">
+          <div className="extraContent">
             <h3>封面图地址</h3>
-            <input type="text" name="设置文章封面图" id="coverImage" />
+            <input
+              type="text"
+              name="设置文章封面图"
+              id="coverImage"
+              onChange={(e) => setCoverImage(e.target.value)}
+              value={coverImage}
+            />
           </div>
-          <div id="coverImage">
-            <h3>封面图地址</h3>
-            <input type="text" name="设置文章封面图" id="coverImage" />
+          <div className="extraContent">
+            <h3>文章标签</h3>
+            <input
+              type="text"
+              name="设置文章标签"
+              id="ArchivesTags"
+              onKeyPress={(e) => handleKeyPress(e)}
+              value={tagsFormText}
+              onChange={(e) => setTagsFromText(e.target.value)}
+            />
+            <div id="tagsGroup">
+              {tags.map((tag, index) => {
+                return (
+                  <div className="tag" key={index}>
+                    {tag}
+                    <CloseOutlined onClick={() => handleDeleteTagFromTagGroup(index)} />
+                  </div>
+                )
+              })}
+            </div>
           </div>
         </div>
       </EditContainer>
@@ -144,12 +197,30 @@ const EditContainer = styled.div`
   }
   #extraInfo {
     margin-top: 2em;
-    div {
+    div.extraContent {
       margin: 1em;
       input {
         border: 1px #d2d2d2 solid;
         padding: 0.25em;
         width: 100%;
+      }
+    }
+    div#tagsGroup {
+      display: flex;
+      flex-wrap: wrap;
+      margin-top: 8px;
+      div.tag {
+        background-color: #e2e2e2;
+        margin-right: 8px;
+        padding: 3px 10px;
+        border-radius: 3px;
+        margin-top: 6px;
+        span {
+          margin-left: 6px;
+          :hover {
+            cursor: pointer;
+          }
+        }
       }
     }
   }
