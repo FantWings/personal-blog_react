cd0eae59a55ee7c2d402ce82b2f85c9f2439b775
FantWings <zhijun@furry.top>
2021-09-02 10:24:17 +0400

    移除不必要的console.log

--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from 'react'
 import { userInfoRespond } from './interfaces'
 
 export const useUserInfo = () => {
+  // 初始化初始状态
   const [data, setData] = useState<userInfoRespond>()
 
   useEffect(() => {
+    // 从localstorge序列化用户信息，返回给调用者
     const loggedInUser = localStorage.getItem('userInfo')
-    console.log('Using LocalStorge UserInfo Data:', loggedInUser)
     if (loggedInUser) setData(JSON.parse(loggedInUser))
   }, [])
 
