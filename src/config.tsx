4b5f8406ee06e2f0aeb7a060fa6bc9bb49fa34ea
FantWings <zhijun@furry.top>
2021-09-12 14:34:27 +0400

    更新环境变量获取方式

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,3 @@
-export const BASEURL = process.env.BASEURL || 'http://127.0.0.1:5000'
-export const BUILD = process.env.BUILD || '1.1.10'
-export const RECORDNUM = process.env.RECORDNUM || ''
+export const BASEURL = process.env.REACT_APP_BASEURL || 'https://127.0.0.1:5000'
+export const BUILD = process.env.REACT_APP_BUILD || undefined
+export const RECORDNUM = process.env.REACT_APP_RECORDNUM || undefined
