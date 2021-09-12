76b647a9805a629228a170067f18a9516befa94b
FantWings <zhijun@furry.top>
2021-09-12 11:07:34 +0400

    build fix

--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,3 @@
 export const BASEURL = process.env.BASEURL || 'http://127.0.0.1:5000'
-export const BUILD = process.env.BUILD || ''
+export const BUILD = process.env.BUILD || '1.1.10'
 export const RECORDNUM = process.env.RECORDNUM || ''
